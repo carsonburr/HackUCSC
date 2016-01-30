d6e8b76d1c79dc3815e2653b6860af0f4126552b
kyjakim <kyjakim@ucsc.edu>
2016-01-30 14:26:58 -0800

    added methods to database
    added the find and toString methods

--- HackUCSC/core/src/com/fourthfloor/hack/utils/Database.java
+++ HackUCSC/core/src/com/fourthfloor/hack/utils/Database.java
@@ -7,9 +7,28 @@ import java.util.ArrayList;
  * Created by Kyle on 1/29/2016.
  */
 public class Database {
+    
+        public static ArrayList<Employee> database = new ArrayList<Employee>();
 
-    ArrayList<Employee> database = new ArrayList<Employee>();
+    //finds specified employee by name
+    public String find(String s){
+        String k = s.toUpperCase();
+        for(int i = 0; i<database.size(); i++){
+            String y = database.get(i).getName();
+            if(y.equals(k)) {
+                return database.get(i).toString();
+            }
+        }
+            return "Employee does not exist in database.";
+    }
 
-    //get employee method
-
+    //prints out the names of all employees in the database
+    @Override
+    public String toString(){
+        String j = "";
+        for(int i = 0; i<= database.size(); i++){
+            j = database.get(i).getName() + "\n";
+        }
+    return j;
+    }
 }
