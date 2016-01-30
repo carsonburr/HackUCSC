4ae7a2a170cc55d3d896936afba46a989dff86cd
carsonburr <carsonwburr@gmail.com>
2016-01-29 17:00:55 -0800

    Basic LibGDX Project
    This is what LibGDX starts you with, a kind of hello world project to
    make sure everything is working

---
+++ HackUCSC/android/src/com/fourthfloor/hack/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fourthfloor.hack.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fourthfloor.hack.MainCore;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainCore(), config);
+	}
+}
---
+++ HackUCSC/desktop/src/com/fourthfloor/hack/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fourthfloor.hack.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fourthfloor.hack.MainCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainCore(), config);
+	}
+}
