e8b98b8ed120848cebef549228e7b9940185be7d
kyjakim <kyjakim@ucsc.edu>
2016-01-30 22:01:56 -0800

    made the addEmployee button work
    finished that sht

--- HackUCSC/core/src/com/fourthfloor/hack/screens/newEPicture.java
+++ HackUCSC/core/src/com/fourthfloor/hack/screens/newEPicture.java
@@ -8,86 +8,70 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
-import com.badlogic.gdx.utils.Align;
 import com.fourthfloor.hack.MainCore;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.fourthfloor.hack.utils.Database;
+import com.fourthfloor.hack.utils.Employee;
 
 /**
  * Created by Kyle on 1/30/2016.
  */
-public class EmployerScreen implements Screen {
-    private String PW;
+public class newEPicture implements Screen {
     private MainCore core;
     private Stage stage;
-
+    TextField txtInput;
     Table list;
-    TextButton findEmployeeButton;
-    TextButton addEmployeeButton;
-    TextButton back;
+    TextButton enterButton;
+    public static String newEPicture;
 
-    public EmployerScreen(MainCore mainCore){
-        PW = "1234";
-        this.core = mainCore;
+    public newEPicture (MainCore mainCore) {
         stage = new Stage();
+        this.core = mainCore;
         list = new Table();
-        list.align(Align.topLeft);
 
         NinePatchDrawable patch = new NinePatchDrawable(new NinePatch(new Texture("ListItem.png"), 1, 1, 1, 1));
 
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         style.down = patch;
         style.up = patch;
-        style.font = new BitmapFont(Gdx.files.internal("Arial_small.fnt"), Gdx.files.internal("Arial_small_0.png"), false);
+        style.font = new BitmapFont(Gdx.files.internal("Arial3.fnt"), Gdx.files.internal("Arial3_0.png"), false);
         style.fontColor = Color.GRAY;
 
-        findEmployeeButton = new TextButton("Find Employee", style);
-        addEmployeeButton = new TextButton("Add Employee", style);
-        back =  new TextButton("Back to main menu", style);
-
         list.setFillParent(true);
-        list.add(findEmployeeButton).top().left().width(Gdx.graphics.getWidth() / 6).height(Gdx.graphics.getHeight() / 10);
+
+        TextField.TextFieldStyle style1 = new TextField.TextFieldStyle();
+        style1.font = new BitmapFont(Gdx.files.internal("Arial3.fnt"), Gdx.files.internal("Arial3_0.png"), false);
+        style1.fontColor = Color.GRAY;
+        style1.background = new NinePatchDrawable(new NinePatch(new Texture("ListItem.png"),1,1,1,1));
+
+        enterButton = new TextButton("Enter", style);
+        txtInput = new TextField("", style1);
+        txtInput.setMessageText("Type in the Employee's picture path here");
+        list.add(txtInput).top().left().width(Gdx.graphics.getWidth()).height(Gdx.graphics.getHeight() / 2);
         list.row();
-        list.add(addEmployeeButton).top().left().width(Gdx.graphics.getWidth() / 6).height(Gdx.graphics.getHeight() / 10);
-        list.row();
-        list.add(back).top().left().width(Gdx.graphics.getWidth() / 6).height(Gdx.graphics.getHeight() / 10);
+        list.add(enterButton);
 
-
-
-        findEmployeeButton.addListener(new ClickListener() {
+        enterButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                core.setScreen(new FindEmployeeAskScreen(core));
-                dispose();
-
-            }
-        });
-
-        back.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
+                newEPicture = txtInput.getText()+ ".png";
+                Database.database.add(new Employee(newESalary.newESalary, newEName.newEName, newEBenefits.newEBenefits, newEPicture));
                 core.setScreen(new MainMenuScreen(core));
                 dispose();
-
             }
         });
-
-        addEmployeeButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                core.setScreen(new newEName(core));
-                dispose();
-
-            }
-        });
-
 
         Gdx.input.setInputProcessor(stage);
         stage.addActor(list);
+
+
     }
+
 
     @Override
     public void show() {
@@ -99,7 +83,6 @@ public class EmployerScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
-
     }
 
     @Override
@@ -126,5 +109,4 @@ public class EmployerScreen implements Screen {
     public void dispose() {
 
     }
-
 }
